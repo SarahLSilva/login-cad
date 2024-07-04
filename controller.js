ccfc2ca630612252da59fc175329b0676cd3e8aa
Sarah <sarah.lorena@alunoescolassociais.org.br>
2024-07-04 09:36:46 -0300

    armazenando em array o nome na tabela

--- controller.js
+++ controller.js
@@ -3,5 +3,25 @@
 function acessar() {
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha =  document.getElementById('loginSenha').value;
-    
+
+    if(!loginEmail || !loginSenha) {
+        alert('favor preencher todos os campos');
+    }else{
+        //alert('campos preenchidos com sucesso');
+        window.location.href = 'cadrasto.html';
+    }
+}
+
+
+// Função que armazena em array nome na tabela de cadastro
+
+var dadosListas = [];
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+       
+    }else{
+        alert('Favor informar o nome para cadastro');
+    }
 }
