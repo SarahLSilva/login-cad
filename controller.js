e07734c327718f7e7df1720d8f88f75dc884efac
Sarah <sarah.lorena@alunoescolassociais.org.br>
2024-07-04 09:01:43 -0300

    adicionando validação de acesso

---
+++ controller.js
@@ -0,0 +1,7 @@
+// validar acesso de tela de login
+
+function acessar() {
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha =  document.getElementById('loginSenha').value;
+    
+}
